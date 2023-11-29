08dcfb09ac1aeb04126cd6e38a74edb03f3482b6
artchen <aechen1202@hotmail.com>
2023-11-30 00:07:03 +0800

    comptroller

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,33 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Comp} from "../src/Governance/Comp.sol";
+import {Comptroller} from "../src/Comptroller.sol";
+import {Unitroller} from "../src/Unitroller.sol";
+import {WhitePaperInterestRateModel} from "../src/WhitePaperInterestRateModel.sol";
+import "forge-std/console.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
-
+    Comptroller public comptroller;
+    Unitroller public unitroller;
+    Comp public comp;
+    Comptroller public unitrollerProxy;
+    WhitePaperInterestRateModel public whitePaperInterestRateModel;
+    address public admin = makeAddr("admin");
+   
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        vm.startPrank(admin);
+        comp = new Comp(admin);
+        comptroller = new Comptroller();
+        unitroller = new Unitroller();
+        unitroller._setPendingImplementation(address(comptroller));
+        unitrollerProxy = Comptroller(address(unitroller));
+        comptroller._become(unitroller);
+        whitePaperInterestRateModel=new WhitePaperInterestRateModel(100,100);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_getReserve() public {
+        console.log(address(comp));
+        console.log(address(comptroller));
     }
 }
